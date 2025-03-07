c08c220a9f36f48bfd8f2f149ae52ef4763e7f26
Marco Aloisi <marco.aloisi@baxenergy.com>
2025-03-07 14:28:49 +0100

    add example

--- example/main.tf
+++ example/main.tf
@@ -1,44 +1,40 @@
 terraform {
   required_providers {
     farsight = {
-      source  = "example.com/local/farsight"
+      source  = "bax-energy/farsight"
       version = "0.1.0"
     }
   }
 }
 
+provider "farsight" {} # needs : OHDEAR_TEAM_ID and OHDEAR_TEAM_ID
+
 provider "farsight" {
-  api_key = var.api_key
-  team_id = var.team_id
+  api_key = var.api_key # or use environment variable OHDEAR_APY_KEY
+  team_id = var.team_id #or use environment variable OHDEAR_TEAM_ID
+  api_url = var.api_url #or use environment variable OHDEAR_API_URL
 }
 
 
-resource "farsight_ohdear_site" "test_code" {
-  url     = "https://farsight-otel-dev.bax.energy/v1/logs"
-  friendly_name = "Dev - Otel"
+resource "farsight_ohdear_site" "example" {
+  url     = "https://yoururl.com"
 
-  checks {
-    uptime        = true
-  }
-  
-  uptime {
-    check_valid_status_codes = ["2*","405"]
-
-  }
-  tags = [ "falsight" , "dev" ]
-  
+  #all checks are enabled by default
 }
 
-resource "farsight_ohdear_site" "test_code2" {
+resource "farsight_ohdear_site" "example2" {
   url     = "https://api-dev.bax.energy/swagger/index.html"
   friendly_name = "Dev - api public"
 
+  #specify which checks to enable
   checks {
     uptime        = true
   }
-  
+
+  #uptime options
   uptime {
     check_max_redirect_count = 2
+    
   }
   tags = [ "falsight" , "dev" ]
   
