a61bc1c7598d7a9cc7784b341217f68ff25c66d7
Marco Aloisi <marco.aloisi@baxenergy.com>
2025-04-04 18:28:55 +0200

    test generate

--- examples/resources/ohdear_site/resource.tf
+++ examples/resources/ohdear_site/resource.tf
@@ -1,23 +1,23 @@
 resource "ohdear_site" "example" {
-  url     = "https://yoururl.com"
+  url = "https://yoururl.com"
 
   #all checks are enabled by default
 }
 
 resource "ohdear_site" "example2" {
-  url     = "https://yoururl.com"
+  url           = "https://yoururl.com"
   friendly_name = "display name on ohdear"
 
   #specify which checks to enable
   checks {
-    uptime        = true
+    uptime = true
   }
 
   #uptime options
   uptime {
     check_max_redirect_count = 2
-    
+
   }
-  tags = [ "tag1" , "tag2" ]
-  
+  tags = ["tag1", "tag2"]
+
 }
